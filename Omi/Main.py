a5c07b776193a05c751cfe43b4c601dbbe23dc36
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-03-17 20:43:49 +0300

    It was organized according to the structure of the systems and webhook structures were added.

--- Omi/Main.py
+++ Omi/Main.py
@@ -1,17 +1,139 @@
+import os
+import json
+import base64
+import pickle
+from google_auth_oauthlib.flow import Flow
+from googleapiclient.discovery import build
+from DB.Database import DatabaseManager, UserRepository
+from flask import Flask, request, redirect, session, abort
+
 from Gmail import Gmail
-from dotenv import load_dotenv
-from Actions import OmiActions
-from OpenAI.EmailClassifier import EmailClassifier
 
-load_dotenv()
+""" \/ -------------- SETUP -------------- \/ """
+#region setup
+app = Flask(__name__)
+app.secret_key = "mailmate.omi-wroom.org"
 
-Gmail = Gmail.GmailClient()
-OmiActions = OmiActions.ActionClient('en')
-EmailClassifier = EmailClassifier()
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("CLIENT_SECRET_FILE")
 
-def main():
-   emails = Gmail.fetch_emails(1)
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.modify",
+    "https://www.googleapis.com/auth/gmail.metadata",
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "openid"]
 
+REDIRECT_URI = "https://mailmate.omi-wroom.org/gmail-callback"
+
+db = DatabaseManager()
+user_repository = UserRepository(db)
+
+flow = Flow.from_client_secrets_file(
+    GOOGLE_CLIENT_SECRET,
+    scopes=SCOPES,
+    redirect_uri=REDIRECT_URI
+)
+#endregion
+
+""" \/ -------------- WEBHOOK FUNCTIONS -------------- \/ """
+#region webhook
+@app.route("/login")
+def login():
+    uid = request.args.get("uid")
+
+    if not uid:
+        return "OPS! There is no UID :(", 400
+
+    session["uid"] = uid
+
+    auth_url, _ = flow.authorization_url(prompt="consent")
+    return redirect(auth_url)
+
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    uid = session.get("uid")
+    if not uid:
+        return "Error: No user session found.", 400
+
+    # region Get Credentials
+    token_path = user_repository.get_credentials(uid)
+    if not token_path:
+        return "Error: No stored credentials.", 400
+
+    with open(token_path, "rb") as token_file:
+        credentials = pickle.load(token_file)
+
+    if not credentials or not credentials.valid:
+        return "Error: No stored credentials.", 400
+    # endregion
+
+    # region core logout
+    session.pop("uid", None)
+    user_repository.delete_user(uid)
+
+    gmail_client = Gmail.GmailClient(uid, credentials=credentials)
+    gmail_client.stop_gmail_listening()
+    # endregion
+
+
+@app.route("/gmail-callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+    credentials = flow.credentials
+
+    uid = session.get("uid")
+    if not uid:
+        return "Error: There is no uid :(", 400
+
+    gmail_client = Gmail.GmailClient(uid, credentials=credentials)
+    gmail_client.start_gmail_listening(new_emails_arrived)
+
+    # region Update database
+    if not os.path.exists("tokens"):
+        os.makedirs("tokens")
+
+    token_path = f"tokens/{uid}.pickle"
+    with open(token_path, "wb") as token_file:
+        pickle.dump(credentials, token_file)
+
+    if user_repository.has_user(uid):
+        user_repository.update_credentials(uid, token_path)
+    else:
+        user_repository.add_user(uid, token_path)
+    # endregion
+
+    return "Login is successful, infos are saved!"
+
+
+@app.route('/pubsub', methods=['POST'])
+def pubsub_push_handler():
+    print(request.get_json())
+    if not request.is_json:
+        abort(400)
+
+    envelope = request.get_json()
+    if not envelope or 'message' not in envelope:
+        abort(400)
+
+    pubsub_message = envelope['message']
+    if 'data' in pubsub_message:
+        message_data = base64.b64decode(pubsub_message['data']).decode('utf-8')
+        notification = json.loads(message_data)
+        print(notification)
+
+        email_id = notification.get('emailAddress')
+        if email_id:
+            print(email_id)
+
+    return '', 204
+#endregion
+
+""" \/ -------------- EMAIL FUNCTIONS -------------- \/ """
+#region mail
+def new_emails_arrived(emails: []):
    for email in emails:
        classify = EmailClassifier.classify_email(email)
        answer = classify["answer"]
@@ -21,4 +143,4 @@ def main():
                print(f"Failed to send email to Omi. HTTP Status: {status_code}")
 
 if __name__ == '__main__':
-    main()
+    app.run(host='127.0.0.1', port=5000, debug=True, ssl_context="adhoc")
