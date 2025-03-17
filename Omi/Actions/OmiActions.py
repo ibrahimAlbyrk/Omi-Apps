f0f66a8a3224e3d77db08c03d178cc374e27751e
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-03-17 19:11:27 +0300

    Added more context for conversations and some minor changes

--- Omi/Actions/OmiActions.py
+++ Omi/Actions/OmiActions.py
@@ -3,38 +3,53 @@ import requests
 from datetime import datetime, timezone
 
 OMI_API_KEY = os.getenv("OMI_API_KEY")
-OMI_USER_ID = os.getenv("OMI_USER_ID")
 OMI_APP_ID = os.getenv("OMI_APP_ID")
 
 
 class ActionClient:
-    def __init__(self, language):
+    def __init__(self, uid, language):
+        self.uid = uid
         self.language = language
 
     def send_email_to_conversations(self, mail, classify=None):
-        url = f"https://api.omi.me/v2/integrations/{OMI_APP_ID}/user/conversations?uid={OMI_USER_ID}"
+        url = f"https://api.omi.me/v2/integrations/{OMI_APP_ID}/user/conversations?uid={self.uid}"
         headers = {
             "Authorization": f"Bearer {OMI_API_KEY}",
             "Content-Type": "application/json"
         }
 
         important = classify['important']
+        sender_importance = classify['sender_importance']
+
+        subject = mail['subject']
+        m_from = mail['from']
+        content = mail['body']
+        priority = classify['priority']
+        sentiment = classify['sentiment']
+        tags = classify['tags']
+        summary = classify['summary']
 
         has_attachments = classify['has_attachment']
         has_links = classify['has_links']
-        reply_required = classify['reply_required']
         suggested_actions = classify['suggested_actions']
+        reply_required = classify['reply_required']
 
         text = "\n\n".join([
-            f"# {mail['subject']}",
-            f"**From**: {mail['from']}",
-            f"**Priority**: {classify['priority']}",
+            f"# {subject}",
+            f"**From**: {m_from}",
+            f"**Priority**: {priority}",
+            f"**Tags**: {tags}",
+            f"**sentiment**: {sentiment}",
+            f"**Sender Importance**: {sender_importance}",
+            f"{f"**Suggested Actions**: {suggested_actions}" if len(suggested_actions) > 0 else ""}",
             "---",
             f"## Summary",
-            f"{classify['summary']}",
+            f"{summary}",
             "---",
+            f"{"(Reply Required)" if reply_required else ""}",
+            f"{"(Links)" if has_links else ""} {"(Attachments)" if has_attachments else ""}",
             f"## Content",
-            f"{mail['body']}"
+            f"{content}"
         ])
 
         data = {
