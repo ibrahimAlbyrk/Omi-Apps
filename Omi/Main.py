05a41d3da091c2b893d280d2dc770f92fb819a32
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-03-20 14:26:05 +0300

    Update Main.py

--- Omi/Main.py
+++ Omi/Main.py
@@ -130,4 +130,6 @@ def start_listening_mail(uid: str, credentials: str):
         max_results=3
     )
 
-start_listening_all_users()
+if __name__ == '__main__':
+    start_listening_all_users()
+    app.run(host='127.0.0.1', port=5000, debug=False, ssl_context="adhoc")
