b101bda3c413bb724a09435edf6927060800caec
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-03-20 11:00:16 +0300

    added logger for debug

--- Omi/new_emails_monitor.py
+++ Omi/new_emails_monitor.py
@@ -1,10 +1,17 @@
+import Logger
+from Logger import FormatterType, LoggerType
 from action_service import OmiActionService
 from classification_service import AIClassificationService
+
+logger = Logger.Manager("Emails Monitor",
+                        FormatterType.ADVANCED,
+                        LoggerType.CONSOLE)
 
 classification_service = AIClassificationService()
 
 
 def process_new_emails(uid: str, emails: []):
+    count: int = 0
     for email in emails:
         classification = classification_service.classify_email(email)
         answer = classification.get("answer", False)
@@ -14,3 +21,7 @@ def process_new_emails(uid: str, emails: []):
             success, status_code = action_service.send_email(email, classification)
             if not success:
                 print(f"Failed to send email to Omi. HTTP Status: {status_code}")
+            else:
+                count += 1
+
+    logger.info(f"Processed {count} new emails")
