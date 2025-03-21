359a861eb0b5e2f028d2791ad1a1d64b366aa4ac
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-03-21 16:09:08 +0300

    Added error codes

--- Omi/Config.py
+++ Omi/Config.py
@@ -1,4 +1,5 @@
 import os
+from enum import Enum
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -21,3 +22,13 @@ GMAIL_SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
 # FILES
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+
+
+# WEBHOOK
+class ErrorResponses(Enum):
+    NO_UID = ("OPS! There is no UID :(", 401)
+    NO_CREDENTIALS("Error: No stored credentials.", 402)
+    NO_VALID_CREDENTIALS("Error: No valid credentials found.", 403)
+    SESSION_EXPIRED = ("Error:This session is over :(", 405)
+    INVALID_DATA = ("Invalid data types", 406)
+    MISSING_UID = ("Missing UID", 407)
